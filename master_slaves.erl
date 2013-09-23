ea19cca2078ce0c8669376b1906d6b7ac3dc4e67
angolinha <xsustek.martin@gmail.com>
2013-09-23 18:28:53 +0200

    Commited 23.09.2013

--- master_slaves.erl
+++ master_slaves.erl
@@ -1,71 +1,106 @@
 -module(master_slaves).
--export([start/1]).
+-export([start/1, master/1, slave_listen/0, to_slave/2]).
 
 start(N) ->
+	flush(),
+	register('output', self()),
+	spawn(master_slaves, master, [N])
+.
+
+master(N) ->
 	flush(), 						%% empty master's mailbox
 	register('master', self()),		%% register first process as master
-	%%List = create_slaves(N),		%% create N slave processes and obtain list od then in form {Pid, order_num}
-	create_slaves(N),
-	master_listen(N)				%% await messages and commands
+	List = create_slaves(N),		%% create N slave processes and obtain list od then in form {Pid, order_num}
+	master_listen(N, List)				%% await messages and commands
 .
 
 create_slaves(N) ->
 	case ( N == 0 ) of
 		true ->
-%%			List = null;									%% at the end of recursion List is empty and waits for additions
-			null;
+			List = {};									%% at the end of recursion List is empty and waits for additions
 		false ->
-			Pid = spawn(master_slaves, slave, [N]),			%% save Pid of created slave process
-%%			List = [ create_slaves(N-1) | { Pid, N } ],		%% form list of previously created slave processes and currently created process
-			register('slave_'++N, Pid)
-	end
-%%	List													%% return list of all created slave processes
-.
-
-slave(N) ->									%% sending dummy messages (code which is executed by every newly created slave process)
-	case N of
-		1 -> 
-			to_slave(3, 'Sprava 1.');
-		2 -> 
-			to_slave(4, 'Sprava 2.');
-		3 -> 
-			to_slave(1, 'Sprava 3.');
-		4 -> 
-			to_slave(2, 'HALT')
-	end
+			Pid = spawn(master_slaves, slave_listen, []),			%% save Pid of created slave process
+			List = [ create_slaves(N-1) | { Pid, N } ]		%% form list of previously created slave processes and currently created process
+	end,
+	List													%% return list of all created slave processes
 .
 
 to_slave(N, Msg) ->
-	master ! { "Msg", N, Msg }		%% send tuple to master which says: "This is message which should be sent to N-th slave."
+	master ! { "Msg", N, Msg },		%% send tuple to master which says: "This is message which should be sent to N-th slave."
+	receive
+		Msg ->
+			io:format("~w ~n",[Msg])
+	end
 .
 
-master_listen(M) ->
+master_listen(M, List) ->
 	receive
 		{ Type, N, Msg } ->												%% await tuple of certain form
 			case Type of
 				"Msg" ->												%% if message should be forwarded
-					case ( Msg == "HALT") of
+					case ( Msg == 'HALT') of
 						true ->											%% if master received "HALT" message it sends "DIE" message to every slave and stops listening
-							shutdown_slaves(M),
-							io:format("Master process going down."),
-							null;
+							shutdown_slaves(List),
+							output ! io_lib:format("Master process going down.~n");
 						false ->										%% or else master forwards message to target slave process and continues listening
-							N ! Msg,
-							master_listen(M)
+							Pid = find_pid(N, List), 
+							case ( Pid == null ) of
+								true ->
+									output ! io_lib:format("~nDidn't find pair Pid to N ~w !~n~n", [N]);
+								false ->
+									Pid ! Msg
+							end,
+							master_listen(M, List)
 					end;
 				"Log" ->												%% if message should be printed
-					io:format("Message: ~w from: ~p ~n", [Msg, N])
+					output ! io_lib:format("Slave ~p got message ~w.~n", [N, Msg]),
+					master_listen(M, List)
 			end
 	end
 .
 
-shutdown_slaves(N) ->				%% recursively shutdown all slaves
-	case ( N == 0 ) of
-		true ->
+find_pid(N, List) ->
+	case List of
+		[] ->						%% if examined list is empty, we do nothing
 			null;
-		false ->
-			shutdown_slaves(N-1),
-			to_slave(N, "DIE")
+		{ PID, M } ->				%% last examined list will be tuple, so we compare Pids and do proper action 
+			case ( M == N ) of
+				true ->
+					PID;
+				false ->
+					null
+			end;
+		[ Head | Tail ] ->
+			case Tail of						%% examine if head of the list is in {Pid, N} form
+				{ Pid, I } ->
+					case ( I == N ) of
+						true -> 
+							Pid;						%% if head is in correct form and order number equals to N, return process's Pid
+						false ->
+							find_pid(N, Head)			%% else continue with searching Tail
+					end;
+				_ ->
+					find_pid(N, Head)			%% first list element is empty tuple, so if we find it, we skip it
+			end
+	end
+.
+
+shutdown_slaves(List) ->				%% recursively shutdown all slaves
+	case List of
+		{} ->
+			null;
+		[] ->
+			null;
+		{ PID, _ } ->
+			PID ! "DIE";
+		[ Head | Tail ] ->
+			case Tail of
+				{ Pid, _ } ->
+					Pid ! "DIE";
+				_ ->
+					null
+			end,
+			shutdown_slaves(Head)
 	end
 .
 
@@ -74,7 +109,7 @@ slave_listen() ->
 		Msg ->
 			case ( Msg == "DIE" ) of
 				true -> 								%% if message is "DIE", process terminates gracefully with its PID as exit message
-					exit(self());
+					exit("Obtained DIE message.");
 				false ->								%% or else it sends received message to master to be printed and continues listening
 					master ! { "Log", self(), Msg },
 					slave_listen()
--- temp.erl
+++ temp.erl
@@ -1,7 +1,22 @@
 -module(temp).
--export([slave/0]).
+-export([start/1, slave/0]).
+
+start(N) ->
+    List = create_slaves(N),        %% create N slave processes and obtain list od then in form {Pid, order_num}
+	io:format("~w ~n", [List])
+.
+
+create_slaves(N) ->
+    case ( N == 0 ) of
+        true ->
+            List = {};                                    %% at the end of recursion List is empty and waits for additions
+        false ->
+            Pid = spawn(temp, slave, []),         %% save Pid of created slave process
+            List = [ create_slaves(N-1) | { Pid, N } ]      %% form list of previously created slave processes and currently created process
+    end,
+    List                                                    %% return list of all created slave processes
+.
 
 slave() ->
-	master ! "It's alive!",
-	exit(0)
+	null
 .
